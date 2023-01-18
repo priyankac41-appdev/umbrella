d91cdbbff208db450f8aef43c76b690176a56e28
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-18 22:04:57 +0000

    new changes

--- my_program.rb
+++ my_program.rb
@@ -35,7 +35,30 @@ loc_hash = geo_hash.fetch("location")
 latitude = loc_hash.fetch("lat")
 longitude = loc_hash.fetch("lng")
 
-p latitude
-p longitude
+# p latitude
+# p longitude
 
 # get stuff from dark sky
+# step 1- get the link
+dark_sky_string = "https://api.darksky.net/forecast/"+ENV.fetch('DARK_SKY_KEY')+"/"+latitude.to_s+","+longitude.to_s
+
+require("open-uri")
+
+# step 2- open/read it
+dark_sky_raw_data = URI.open(dark_sky_string).read
+
+require("json")
+
+# step 3- parse it
+parsed_data = JSON.parse(dark_sky_raw_data)
+#p parsed_data.keys
+
+#p parsed_data.fetch("hourly").fetch("data")[0]
+
+# {"time"=>1674075600, "summary"=>"Overcast", "icon"=>"cloudy", "precipIntensity"=>0, "precipProbability"=>0, "temperature"=>36.69, "apparentTemperature"=>32.6, "dewPoint"=>31.06, "humidity"=>0.8, "pressure"=>1015.1, "windSpeed"=>4.98, "windGust"=>7.54, "windBearing"=>78, "cloudCover"=>0.98, "uvIndex"=>0, "visibility"=>10, "ozone"=>331.5}
+#p parsed_data.fetch("hourly").fetch("data").length
+
+for x in 0..(parsed_data.fetch("hourly").fetch("data").length-1) do
+  time_at_x = Time.at(parsed_data.fetch("hourly").fetch("data")[x].fetch("time"))
+  p time_at_x
+end
